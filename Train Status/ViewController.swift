08178e157eefe193c0db6cbbca6d6ae0d4eec10f
Garry Yeung <garry0325@gmail.com>
2020-03-12 12:58:06 +0800

    Initial Commit

---
+++ Train Status/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Train Status
+//
+//  Created by Garry Yeung on 2020/3/12.
+//  Copyright © 2020 Garry Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
