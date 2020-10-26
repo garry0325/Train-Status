5cf16a9beb7a4f6ed01abe929d17056acda943f0
Garry Yeung <garry0325@gmail.com>
2020-10-26 15:44:28 +0800

    Make MOTCQuery shared instance.

--- Train Status/MOTCQuery.swift
+++ Train Status/MOTCQuery.swift
@@ -14,26 +14,31 @@ class MOTCQuery {
 	let timeoutForRequest = 10.0
 	let timeoutForResource = 15.0
 	
-	let stationCode: String
+	static let shared = MOTCQuery()
 	
 	let appID = "1baabcfdb12a4d88bd4b19c7a2c3fd23"
 	let appKey = "4hYdvDltMul8kJTyx2CbciPeM1k"
 	
-	let authTimeString: String!
-	let authorization: String!
+	var authDateFormatter = DateFormatter()
+	var authTimeString: String!
+	var authorization: String!
 	
 	var urlConfig = URLSessionConfiguration.default
 	
-	init(stationCode: String) {
-		self.stationCode = stationCode
+	init() {
+		print("MOTC initialized")
+		authDateFormatter = DateFormatter()
+		authDateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ww zzz"
+		authDateFormatter.locale = Locale(identifier: "en_US")
+		authDateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
 		
+		urlConfig.timeoutIntervalForRequest = timeoutForRequest
+		urlConfig.timeoutIntervalForResource = timeoutForResource
+	}
+	
+	func authentication() {
 		//	Prepare authentication for MOTC website
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ww zzz"
-		dateFormatter.locale = Locale(identifier: "en_US")
-		dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-		
-		self.authTimeString = dateFormatter.string(from: Date())
+		self.authTimeString = authDateFormatter.string(from: Date())
 		
 		let signDate = String(format: "x-date: %@", self.authTimeString)
 		let key = SymmetricKey(data: Data(self.appKey.utf8))
@@ -41,12 +46,9 @@ class MOTCQuery {
 		let base64HmacString = Data(hmac).base64EncodedString()
 		
 		self.authorization = "hmac username=\"\(self.appID)\", algorithm=\"hmac-sha256\", headers=\"x-date\", signature=\"\(base64HmacString)\""
-		
-		urlConfig.timeoutIntervalForRequest = timeoutForRequest
-		urlConfig.timeoutIntervalForResource = timeoutForResource
 	}
 	
-	func queryStationBoard() -> [Train] {
+	func queryStationBoard(stationCode: String) -> [Train] {
 		var trainList: [Train] = []
 		let semaphore = DispatchSemaphore(value: 0)
 		
@@ -58,7 +60,9 @@ class MOTCQuery {
 		formatter2.dateFormat = "yyyy-MM-dd"
 		formatter2.timeZone = TimeZone(abbreviation: "UTC+8")
 		
-		let url = URL(string: "https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/LiveBoard/Station/\(self.stationCode)?$format=JSON")!
+		authentication()
+		
+		let url = URL(string: "https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/LiveBoard/Station/\(stationCode)?$format=JSON")!
 		var request = URLRequest(url: url)
 		request.setValue(authTimeString, forHTTPHeaderField: "x-date")
 		request.setValue(authorization, forHTTPHeaderField: "Authorization")
@@ -141,13 +145,16 @@ class MOTCQuery {
 				}
 				else {
 					print("Response not 200")
+					DispatchQueue.main.async {
+						ErrorAlert.presentErrorAlert(title: "網路錯誤 \(response.statusCode)", message: "請稍後再試")
+					}
 				}
 			}
 			semaphore.signal()
 		}
 		
 		// Due to some specific trains do not show up from the API above, the following API is used to compensate
-		let url2 = URL(string: "https://ptx.transportdata.tw/MOTC/v3/Rail/TRA/DailyStationTimetable/Today/Station/\(self.stationCode)?$select=Direction%2C%20TimeTables&$format=JSON")!
+		let url2 = URL(string: "https://ptx.transportdata.tw/MOTC/v3/Rail/TRA/DailyStationTimetable/Today/Station/\(stationCode)?$select=Direction%2C%20TimeTables&$format=JSON")!
 		request = URLRequest(url: url2)
 		request.setValue(authTimeString, forHTTPHeaderField: "x-date")	// this line should not be excluded
 		request.setValue(authorization, forHTTPHeaderField: "Authorization")	// this line should not be excluded
@@ -155,7 +162,9 @@ class MOTCQuery {
 		let task2 = session2.dataTask(with: request) { (data, response, error) in
 			if let error = error {
 				print("MOTC query error: \(error.localizedDescription)")
-				
+				DispatchQueue.main.async {
+					ErrorAlert.presentErrorAlert(title: "網路錯誤", message: "網路連線不穩 請稍後再試")
+				}
 			}
 			else if let response = response as? HTTPURLResponse,
 					let data = data {
@@ -222,6 +231,12 @@ class MOTCQuery {
 							let depart = (interval <= -30) ? true:false
 							trainList.append(Train(type: trainType, number: trainNumber, ending: endingStation, direction: direction, line: "", departure: departureTime!, delay: 0, degreeOfIndicator: degree, departed: depart))
 						}
+					}
+				}
+				else {
+					print("Response not 200")
+					DispatchQueue.main.async {
+						ErrorAlert.presentErrorAlert(title: "網路錯誤 \(response.statusCode)", message: "請稍後再試")
 					}
 				}
 			}
